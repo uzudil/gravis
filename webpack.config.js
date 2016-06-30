27493b63cfca047b6e9fdd1cf92a371a0373c5ba
uzudil <hunhm1@gmail.com>
2016-06-30 13:32:47 -0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,6 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx'],
-		modulesDirectories: ["./src", "node_modules", "bower_components"]
+		modulesDirectories: ["./src", "node_modules"]
 	}
 };
